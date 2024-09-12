43c2f9d9ca085e4454f967e7c5b00cc53901a29d
park-su-hyeon <162524707+park-su-hyeon@users.noreply.github.com>
2024-09-12 10:54:21 +0900

    Add files via upload

---
+++ 2-treestacktraversal.c
@@ -0,0 +1,48 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+typedef TreeNode* element;
+typedef struct TreeNode {
+	element data;
+	struct TreeNode* left, * right;
+} TreeNode;
+
+int main() {
+	TreeNode* root = ;
+	root->data = 1;
+
+	GenerateLinkTree(root);
+
+	Linkorders(root);
+
+	return 0;
+}
+
+void PlaceNode(TreeNode* node, int direction, int data) {
+	if (&direction == 0) {
+		pop(&s, );
+		 = ->left;
+	}
+}
+
+void GenerateLinkTree(TreeNode* root, int direction, int data) {
+	PlaceNode(root, 0, 2);
+}
+
+void LinkOrders(TreeNode* root) {
+	LinkPreOrder(root);
+	LinkInOrder(root);
+	LinkPostOrder(root);
+}
+
+void LinkPreOrder(TreeNode* root) {
+
+}
+
+void LinkInOrder(TreeNode* root) {
+
+}
+
+void LinkPostOrder(TreeNode* root) {
+
+}
