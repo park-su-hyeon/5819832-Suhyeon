678ef4a423f38b64d0736ebcbe20151c6711dbcc
park-su-hyeon <162524707+park-su-hyeon@users.noreply.github.com>
2024-10-19 21:36:36 +0900

    6-sortbyheap.c

--- 6-sortbyheap.c
+++ 6-sortbyheap.c
@@ -1,67 +1,80 @@
-﻿#include <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 
 void swap(int* a, int* b) {
-	int temp = *a;
-	*a = *b;
-	*b = temp;
+    int temp = *a;
+    *a = *b;
+    *b = temp;
 }
 
-void resortHeap(int inputData[], int n, int i) {
-	int largest = i; // 루트를 가장 큰 값으로 설정
-	int left = 2 * i + 1; // 왼쪽 자식
-	int right = 2 * i + 2; // 오른쪽 자식
+// 최대 힙을 구성하는 함수
+void heapify(int arr[], int n, int i) {
+    int largest = i; // 루트
+    int left = 2 * i + 1; // 왼쪽 자식
+    int right = 2 * i + 2; // 오른쪽 자식
 
-	// 왼쪽 자식이 더 큰 경우
-	if (left < n && inputData[left] > inputData[largest]) {
-		largest = left;
-	}
+    // 왼쪽 자식이 루트보다 큰 경우
+    if (left < n && arr[left] > arr[largest])
+        largest = left;
 
-	// 오른쪽 자식이 더 큰 경우
-	if (right < n && inputData[right] > inputData[largest]) {
-		largest = right;
-	}
+    // 오른쪽 자식이 현재 최대보다 큰 경우
+    if (right < n && arr[right] > arr[largest])
+        largest = right;
 
-	// 루트가 가장 큰 값이 아닐 경우 교환하고 재귀 호출
-	if (largest != i) {
-		swap(&inputData[i], &inputData[largest]);
-		resortHeap(inputData, n, largest);
-	}
+    // 최대가 루트가 아닌 경우
+    if (largest != i) {
+        swap(&arr[i], &arr[largest]);
+        // 힙을 재귀적으로 정렬
+        heapify(arr, n, largest);
+    }
 }
 
-void BuildMaxHeap(int inputData[], int n) {
-	for (int i = n / 2 - 1; i >= 0; i--) {
-		resortHeap(inputData, n, i);
-	}
+// 최대 힙을 생성하는 함수
+void BuildMaxHeap(int arr[], int n) {
+    // 마지막 비어있는 노드의 부모 노드부터 힙을 구성
+    for (int i = n / 2 - 1; i >= 0; i--)
+        heapify(arr, n, i);
 }
 
+// 힙 정렬 및 상태 출력을 위한 함수
 void BuildMaxHeapAndSort(int inputData[], int n) {
-	// max heap 생성하는 함수
-	BuildMaxHeap(inputData, n);
-	// 하나씩 힙에서 루트(최대값)를 제거하고 정렬
-	for (int i = n - 1; i > 0; i--) {
-		swap(&inputData[0], &inputData[i]);
-		resortHeap(inputData, i, 0);
-		// 힙 재정렬 후 상태 출력
-		for (int j = 0; j < i; j++) {
-			printf("%d ", inputData[j]);
-		}
-		printf("\n");
-	}
-}
+    BuildMaxHeap(inputData, n);
 
+    // 힙 상태 출력
+    for (int i = 0; i < n; i++) {
+        printf("%d ", inputData[i]);
+    }
+    printf("\n");
+
+    // 하나씩 힙에서 루트(최대값)를 제거하고 정렬
+    for (int i = n - 1; i > 0; i--) {
+        // 루트와 마지막 요소 교환
+        swap(&inputData[0], &inputData[i]);
+        // 힙 크기를 줄이고 재정렬
+        heapify(inputData, i, 0);
+
+        // 힙 재정렬 후 상태 출력
+        for (int j = 0; j < i; j++) {
+            printf("%d ", inputData[j]);
+        }
+        printf("\n");
+    }
+}
 
 int main() {
-	int inputData[] = { 34, 12, 76, 59, 32, 55, 88, 26, 16, 79, 35, 85, 29, 78, 41, 56, 86 };
-	int size = sizeof(inputData) / sizeof(inputData[0]);
-	// 힙 정렬 실행
-	printf("<Sort>\n");
-	BuildMaxHeapAndSort(inputData, size);
-	// 정렬된 결과 출력
-	printf("\nSorted Array:\n");
-	for (int i = 0; i < size; i++) {
-		printf("%d ", inputData[i]);
-	}
-	printf("\n");
-	return 0;
+    int inputData[] = { 34, 12, 76, 59, 32, 55, 88, 26, 16, 79, 35, 85, 29, 78, 41, 56, 86 };
+    int size = sizeof(inputData) / sizeof(inputData[0]);
+
+    // 힙 정렬 실행
+    printf("<Sort>\n");
+    BuildMaxHeapAndSort(inputData, size);
+
+    // 정렬된 결과 출력
+    printf("\nSorted Array:\n");
+    for (int i = 0; i < size; i++) {
+        printf("%d ", inputData[i]);
+    }
+    printf("\n");
+
+    return 0;
 }
