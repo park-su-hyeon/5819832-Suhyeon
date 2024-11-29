e43785ea5c257eaaf66e74679027141e5fd7c5cb
park-su-hyeon <162524707+park-su-hyeon@users.noreply.github.com>
2024-11-29 19:19:04 +0900

    13-mergesort.c

--- 13-mergesort/13-mergesort.c
+++ 13-mergesort/13-mergesort.c
@@ -1,7 +1,6 @@
 ﻿#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <windows.h> 
 
 #define SIZE 100
 #define RUNS 20
@@ -21,7 +20,7 @@ void printArray(int array[], int size) {
 }
 
 void printPartialArray(int array[], int size) {
-    if (rounds % 10 == 0 && isFirst == 0) { //10번에 한번만 출력
+    if (comparisonCount % 10 == 0 && isFirst == 0) { //10번에 한번만 출력
         for (int i = 0; i < 10; i++) //0 ~ 9값
             printf("%3d ", array[i]);
         printf("| ");
@@ -29,60 +28,55 @@ void printPartialArray(int array[], int size) {
             printf("%3d ", array[i]);
         printf("\n\n");
     }
+}
 
 void merge(int list[], int left, int mid, int right) {
-        int i = left, j = mid + 1, k = left;
+    int i = left, j = mid + 1, k = left;
 
-        while (i <= mid && j <= right) {
-            comparisonCount++;
-            if (list[i] <= list[j]) {
-                sorted[k++] = list[i++];
-                moveCount++;
-            }
-            else {
-                sorted[k++] = list[j++];
-                moveCount++;
-            }
-        }
-
-        if (i > mid) {
-            for (int l = j; l <= right; l++) {
-                sorted[k++] = list[l];
-                moveCount++;
-            }
+    while (i <= mid && j <= right) {
+        comparisonCount++;
+        if (list[i] <= list[j]) {
+            sorted[k++] = list[i++];
+            moveCount++;
         }
         else {
-            for (int l = i; l <= mid; l++) {
-                sorted[k++] = list[l];
-                moveCount++;
-            }
-        }
-
-        for (int l = left; l <= right; l++) {
-            list[l] = sorted[l];
+            sorted[k++] = list[j++];
             moveCount++;
         }
     }
 
+    if (i > mid) {
+        for (int l = j; l <= right; l++) {
+            sorted[k++] = list[l];
+            moveCount++;
+        }
+    }
+    else {
+        for (int l = i; l <= mid; l++) {
+            sorted[k++] = list[l];
+            moveCount++;
+        }
+    }
+
+    for (int l = left; l <= right; l++) {
+        list[l] = sorted[l];
+        moveCount++;
+    }
+}
 
 void doMergeSort(int list[], int left, int right) {
-    static int round = 0;  // for tracking rounds
-    if (left < right) {
-        int mid = (left + right) / 2;
-        doMergeSort(list, left, mid);
-        doMergeSort(list, mid + 1, right);
-        merge(list, left, mid, right);
-
-        round++;
-
-        // 10번에 한번씩 배열 출력
-        if (round % 10 == 0 && isFirst == 0) {
-            printPartialArray(list, SIZE);
+    for (int width = 1; width < right - left + 1; width = 2 * width) {
+        for (int i = left; i < right; i += 2 * width) {
+            int mid = i + width - 1;
+            int end = (i + 2 * width - 1) < right ? (i + 2 * width - 1) : right;
+            if (mid < right) {
+                merge(list, i, mid, end);
+            }
         }
     }
 }
 
-void generateRandomNumbers(int array[]) {
+void generateRandomArray(int array[]) {
     for (int i = 0; i < SIZE; i++) {
         array[i] = rand() % 1000; // 0~999 사이의 랜덤 값
     }
@@ -95,7 +89,7 @@ int main() {
 
     // 20회 동안 랜덤 배열을 생성하고 정렬하며 비교횟수 및 이동횟수를 계산
     for (int i = 0; i < 20; i++) {
-        generateRandomNumbers(array); // 랜덤 배열 생성
+        generateRandomArray(array); // 랜덤 배열 생성
         comparisonCount = 0;
         moveCount = 0;
 
@@ -122,4 +116,3 @@ int main() {
 
     return 0;
 }
-
